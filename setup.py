868f56ee114b05e75ca467d7e551d1918a236430
Sina Esmaeili <esmaeili.sina@gmail.com>
2023-06-26 14:39:53 -0700

    Initialized the repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a demo repo for developing GPT model from scratch',
+    author='Sina Esmaeili',
+    license='',
+)
